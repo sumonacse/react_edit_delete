c3043a7854e93d32c6b934421e525fdad39f8c7e
sumonacse <ssria.cse@gmail.com>
2023-03-23 13:08:19 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState, useEffect } from 'react';
+import {Button, Form, Container, ListGroup, Modal} from 'react-bootstrap';
+import { getDatabase, ref, set, push, onValue, remove, update} from "firebase/database";
+
 
 function App() {
+
+  const db = getDatabase();
+  let [firstname,setFirstname] = useState("");
+  let [tasklist,setTasklist] = useState([]);
+  const [show, setShow] = useState(false);
+  const [id, setId] = useState(false);
+
+
+  const handleShow = (id) =>{
+    setShow(true);
+    setId(id);
+  };
+
+  const handleClose = () =>{
+    setShow(false);
+    update(ref(db,"todo/"+id),{
+      task:firstname,
+    })
+  };
+
+
+
+  let handleForm = (e)=>{
+    e.preventDefault();
+
+    set (push(ref(db, 'todo')) , {
+      task : firstname,
+    });
+  }; 
+
+
+  let handleFirstName = (e)=>{
+    setFirstname(e.target.value);
+   
+  };
+  
+  useEffect(()=>{
+    onValue(ref(db, 'todo'), (snapshot) => {
+       let arr=[];
+       snapshot.forEach(item=>{
+        let task={
+          id:item.key,
+          task:item.val(),
+        };
+           arr.push(task);
+      }); 
+      setTasklist(arr);
+    });
+
+  },[]);
+
+ let handleDelete = (id)=>{
+    remove(ref(db,"todo/"+id));
+ };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+        <h1>TODO APP</h1>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Your task</Form.Label>
+              <Form.Control onChange={handleFirstName} type="text"  />
+            </Form.Group>
+           
+            <Button onClick={handleForm} variant="primary" type="submit"> Submit </Button>
+        </Form>
+        <br/>
+        <br/>
+        <br/>
+
+        <ListGroup>
+            {tasklist.map(item =>(
+                 <ListGroup.Item> {item.task.task}  <br/>
+                    <Button variant="info" onClick={()=>handleShow(item.id)}>Edit</Button>{' '}
+                    <Button onClick={()=> handleDelete(item.id)} variant="danger">Delete</Button>{' '}
+                 </ListGroup.Item>
+            ))}
+        </ListGroup>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit your task</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Your task</Form.Label>
+                  <Form.Control onChange={handleFirstName} type="text"  />
+              </Form.Group>  
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Update
+            </Button>
+          </Modal.Footer>
+        </Modal>
+    </Container>
   );
 }
 
+
+
 export default App;
+
+
+
---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcCvf8FBycZYF2pxQ6Q5BnN_V0UDFgdBc",
+  authDomain: "todo-b5655.firebaseapp.com",
+  projectId: "todo-b5655",
+  storageBucket: "todo-b5655.appspot.com",
+  messagingSenderId: "646656672308",
+  appId: "1:646656672308:web:41d38fa3b40b133e86c061"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
